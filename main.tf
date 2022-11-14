932c0914a9fc349e4dc9a071d2e5a0621878fdac
ayoola.akinsol <ayoola.akinsol@vodafone.com>
2022-11-14 20:47:19 +0000

    changes

---
+++ main.tf
@@ -0,0 +1,6 @@
+String token = "rO0ABXQAVklmIHlvdSBkZXNlcmlhbGl6ZSBtZSBkb3duLCBJIHNoYWxsIGJlY29tZSBtb3JlIHBvd2VyZnVsIHRoYW4geW91IGNh..."
+
+rovider "azuredevops" {
+  org_service_url       = "https://dev.azure.com/vfuk-digital"
+  personal_access_token = "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"
+}
